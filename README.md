9d4446edd67b66a696c3df813473bbf16a05d729
miguel oliveira <mo736356@gmail.com>
2024-01-24 17:42:24 -0300

    first commit :tada:

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# curly-carnival
+# Desafio feito pelo [**Felipe**](https://github.com/felipeAguiarCode) da **DIO**
+
+Este desafio tem como Objetivo Classificar o nível de Heróis.
+
+# 📔 Lista de base para decidir os níveis
+
+1. Se XP for menor do que 1.000 = Ferro
+1. Se XP for entre 1.001 e 2.000 = Bronze
+1. Se XP for entre 2.001 e 5.000 = Prata
+1. Se XP for entre 5.001 e 7.000 = Ouro
+1. Se XP for entre 7.001 e 8.000 = Platina
+1. Se XP for entre 8.001 e 9.000 = Ascendente
+1. Se XP for entre 9.001 e 10.000= Imortal
+1. Se XP for maior ou igual a 10.001 = Radiante
---
+++ script.js
@@ -0,0 +1,47 @@
+// Se XP for menor do que 1.000 = Ferro
+// Se XP for entre 1.001 e 2.000 = Bronze
+// Se XP for entre 2.001 e 5.000 = Prata
+// Se XP for entre 5.001 e 7.000 = Ouro
+// Se XP for entre 7.001 e 8.000 = Platina
+// Se XP for entre 8.001 e 9.000 = Ascendente
+// Se XP for entre 9.001 e 10.000= Imortal
+// Se XP for maior ou igual a 10.001 = Radiante
+
+let nameHero = "DeadPool"
+
+let xp = 10000
+
+let rankingHero;
+
+// Eu decidi colocar o valor 1000 como ranking Ferro só pra ter um ranking, e não ser undefined
+
+if (xp <= 1000) {
+  rankingHero = 'Ferro'
+}
+else if(xp >= 10001){
+  rankingHero = 'Radiante'
+}
+else{
+  switch(true){
+    case ((xp > 1000) && (xp < 2001)):
+      rankingHero = 'Bronze'
+      console.log(xp)
+    break
+    case ((xp > 2000) && (xp < 5001)):
+      rankingHero = 'Prata'
+    break
+    case ((xp > 5000) && (xp < 7001)):
+      rankingHero = 'Ouro'
+    break
+    case ((xp > 7000) && (xp < 8001)):
+      rankingHero = 'Platina'
+    break
+    case ((xp > 8000) && (xp < 9001)):
+      rankingHero = 'Ascendente'
+    break
+    case ((xp > 9000) && (xp < 10001)):
+      rankingHero = 'Imortal'
+    break
+  }
+}
+console.log('O Herói de nome ' + nameHero + ' está no nível de ' + rankingHero) 
